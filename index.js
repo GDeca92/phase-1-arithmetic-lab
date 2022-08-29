31deaa1f575facce6ab226a9eefb82a7227a44d8
Gian Carlo De Castro <Giancld92@gmail.com>
2022-08-29 17:53:14 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+let random = +3
+
+let num3 = 24
+let num4 = 6
+let mod = num3 / num4
+
+let max = (0,5,10,15,20)
